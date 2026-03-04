d60606de57e842a005589015a66c60978e14f081
Gabriel Panca Ribeiro <pancabiel@gmail.com>
2026-03-03 22:41:30 -0300

    Revert "CORS fix test"
    This reverts commit 7669045906cfd993c80f5e5bd12517c5fd724644.

--- icake-api/src/main/java/com/icake/config/CorsGlobalConfiguration.java
+++ icake-api/src/main/java/com/icake/config/CorsGlobalConfiguration.java
@@ -6,14 +6,14 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsGlobalConfiguration {
 
 	@Bean
-	public CorsFilter corsFilter() {
+	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfig = new CorsConfiguration();
 
 		corsConfig.setAllowedOrigins(List.of(
@@ -29,6 +29,6 @@ public class CorsGlobalConfiguration {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfig);
 
-		return new CorsFilter(source);
+		return source;
 	}
 }
